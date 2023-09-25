baff9b4f0dadbe50fd75eeebb3fb1a4db555eea0
intaniger <tanakornp@cleversecorp.com>
2023-09-25 23:19:53 +0700

    feat: :sparkles: added union demo code

---
+++ src/unions.ts
@@ -0,0 +1,21 @@
+const isTrue: (value: string | boolean) => boolean = (value) => {
+  if (typeof value === 'string') {
+    return value === 'true'
+  }
+  return value
+}
+
+console.log(isTrue('true'))
+
+console.log(isTrue('false'))
+
+console.log(isTrue('เอิ้ว'))
+
+console.log(isTrue('Men'))
+
+
+console.log(isTrue(true))
+
+console.log(isTrue(false))
+
+console.log(isTrue(3))
