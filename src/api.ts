b2c692b6662f825a706070ca82bea774c8eec6b3
intaniger <tanakornp@cleversecorp.com>
2023-09-28 19:29:21 +0700

    refactor: :sparkles: improved demo code explanation and formatting

--- src/api.ts
+++ src/api.ts
@@ -16,29 +16,88 @@ const getContents = async () => {
     )
     const contentList = response.data.data
 
-    const filteredContent = contentList.filter(
-      (content) =>
-        Number(new Date(content.createdAt)) <
-        Number(new Date('2023-05-22T00:00:00.000+07:00')),
+    const academyFirstDate = new Date('2023-05-22T00:00:00.000+07:00')
+
+    /**
+     * Approach #0 - Back to basic, use naive for loop
+     */
+
+    const filteredResult = []
+    for (let index = 0; index < contentList.length; index++) {
+      const element = contentList[index]
+
+      const createdAtDate = new Date(element.createdAt)
+      if (
+        // using valueOf method
+        createdAtDate.valueOf() < academyFirstDate.valueOf() &&
+        // or using + sign
+        +createdAtDate < +academyFirstDate &&
+        // or just compare two Date
+        createdAtDate < academyFirstDate
+      )
+        filteredResult.push(element)
+    }
+
+    /**
+     * Approach #1: Map each content.createdAt from string to Date first,
+     * and then filter only content that was created before 22-05-2023
+     */
+    const mappedToDate = contentList
+      // Map from string => Date
+      .map((
+        /* Object desctruction, we only need [createdAt] field and left the rest being [others] */ {
+          createdAt,
+          ...others
+        },
+      ) => ({
+        ...others,
+        createdAt: new Date(createdAt),
+      }))
+
+    // And filter only contents that were created before 22-05-2023
+    const filteredContentUsingMapped = mappedToDate.filter(
+      ({ createdAt }) => createdAt.valueOf() < academyFirstDate.valueOf(),
     )
 
-    // const result = []
-    // for (let index = 0; index < contentList.length; index++) {
-    //   const element = contentList[index]
+    // We can also make this work by + as a prefix
+    const shorterFilteredContentUsingMapped = mappedToDate.filter(
+      ({ createdAt }) => +createdAt < +academyFirstDate,
+    )
 
-    //   const dated = new Date(element.createdAt)
-    //   if (dated.valueOf() < new Date('2023-05-22T00:00:00.000+07:00').valueOf())
-    //     result.push(element)
-    // }
+    // Or, even comparing Date without a + sign
+    const shortestFilteredContentUsingMapped = mappedToDate.filter(
+      ({ createdAt }) => createdAt < academyFirstDate,
+    )
 
-    console.log(filteredContent)
+    /**
+     * Approach #2: put cast logic within filter, no map required - no wasting CPU time
+     */
+    const filteredContentWithOnlyFilter = contentList.filter(
+      (content) =>
+        new Date(content.createdAt).valueOf() < academyFirstDate.valueOf(),
+    )
 
-    // const filteredContent = contentList.map(({ createdAt, ...others }) => ({
-    //   ...others,
-    //   createdAt: new Date(createdAt),
-    // })).filter(({createdAt}) => createdAt.valueOf() < (new Date("2023-05-22T00:00:00.000+07:00")).valueOf())
+    // just using + sign
+    const shorterFilteredContentWithOnlyFilter = contentList.filter(
+      (content) => +new Date(content.createdAt) < +academyFirstDate,
+    )
 
-    // console.log(JSON.stringify(filteredContent, null, 2))
+    // Or comparing Date directly
+    const shortestFilteredContentWithOnlyFilter = contentList.filter(
+      (content) => new Date(content.createdAt) < academyFirstDate,
+    )
+
+    console.log(
+      filteredResult.length,
+      filteredContentUsingMapped.length,
+      shorterFilteredContentUsingMapped.length,
+      shortestFilteredContentUsingMapped.length,
+      filteredContentWithOnlyFilter.length,
+      shorterFilteredContentWithOnlyFilter.length,
+      shortestFilteredContentWithOnlyFilter.length,
+    )
+
+    console.log(JSON.stringify(filteredResult, null, 2))
   } catch (error) {
     console.error(error)
   }
